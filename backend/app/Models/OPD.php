be2d0356ab17439df03b5dad95e70e5d9030074a
giancarloys <sinducarlo@gmail.com>
2026-06-26 16:55:30 +0700

    fix verif

--- backend/app/Models/OPD.php
+++ backend/app/Models/OPD.php
@@ -8,8 +8,5 @@ class OPD extends Model
 {
     protected $table = 'opds';
 
-    // 🎯 TAMBAHKAN BARIS INI AGAR LARAVEL TIDAK MENCARI KOLOM 'id'
-    protected $primaryKey = 'id_opd';
-
     protected $fillable = ['nama_opd', 'alamat_opd'];
 }
